736c60638ea97355ea040f313b4cfe29b6544428
rano667 <71762466+rano667@users.noreply.github.com>
2023-03-25 13:34:11 +0530

    news app static for hosting

---
+++ src/components/News.js
@@ -0,0 +1,144 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
+
+export class News extends Component {
+  static defaultProps = {
+    country: "in",
+    pageSize: 5,
+    catagory: "general",
+  };
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+  constructor() {
+    super();
+
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+    };
+  }
+
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&pagesize=${this.props.pageSize}&page=1&category=${this.props.category}&apiKey=e5b64a060ead45adbbbc9fdac3c41cdd`;
+
+    this.setState({
+      loading: true,
+    });
+
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
+  }
+
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      this.props.country
+    }&page=${this.state.page - 1}&pagesize=${this.props.pageSize}&category=${
+      this.props.category
+    }&apiKey=e5b64a060ead45adbbbc9fdac3c41cdd`;
+    this.setState({
+      loading: true,
+    });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+      loading: false,
+    });
+  };
+
+  handleNextClick = async () => {
+    if (
+      !(
+        this.state.page + 1 >
+        Math.ceil(this.state.totalResults / this.props.pageSize)
+      )
+    ) {
+      let url = `https://newsapi.org/v2/top-headlines?country=${
+        this.props.country
+      }&page=${this.state.page + 1}&pagesize=${this.props.pageSize}&category=${
+        this.props.category
+      }&apiKey=e5b64a060ead45adbbbc9fdac3c41cdd`;
+
+      this.setState({
+        loading: true,
+      });
+
+      let data = await fetch(url);
+      let parsedData = await data.json();
+
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+        loading: false,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <div className="container my-3">
+          <h1 className="text-center">NewsMonkey - Top Headlines</h1>
+          {this.state.loading && <Spinner />}
+          <div className="row">
+            {!this.state.loading &&
+              this.state.articles.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <NewsItem
+                      title={element.title ? element.title : ""}
+                      description={
+                        element.description ? element.description : ""
+                      }
+                      imageUrl={
+                        element.urlToImage
+                          ? element.urlToImage
+                          : "https://i.ndtvimg.com/i/2018-03/india-china-border-doklam_650x400_81522061523.jpg"
+                      }
+                      newsUrl={element.url}
+                    />
+                  </div>
+                );
+              })}
+          </div>
+          <div className="container d-flex justify-content-between">
+            <button
+              type="button"
+              disabled={this.state.page <= 1}
+              className="btn btn-dark"
+              onClick={this.handlePrevClick}
+            >
+              &laquo; Previous
+            </button>
+            <button
+              type="button"
+              disabled={
+                this.state.page + 1 >
+                Math.ceil(this.state.totalResults / this.props.pageSize)
+              }
+              className="btn btn-dark"
+              onClick={this.handleNextClick}
+            >
+              Next &raquo;
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default News;
